bc0311a786f2dea2a8330c7a3c677b93645c8672
Julliane Tampus <julliane.tampus123@gmail.com>
2023-12-17 02:18:33 +0800

    first commit iNnounce

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT
+const app = express()
+
+app.use(express.static(path.join(__dirname , 'public')))
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
